b58f850c61f482b5c602629f6fdcdb6b2db3b7f1
haewonK <kaagg7189@naver.com>
2024-02-04 01:50:24 +0900

    answer

--- do it study_1.cpp
+++ do it study_1.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 //백준 11720 두잇1번
 int main(void) {
-    string numbers;
-    int n;
-    cin>>n;
-    cin>>numbers;
-    int sum=0;
-
-    for(int i=0;i<n;i++){
-        sum+=numbers[i]-'0';
-    }
-    cout<<sum<<endl;
-    return 0;
+   int n=0;
+   string numbers;
+   int sum=0;
+   cin>>n;
+   cin>>numbers;
+   for(int i=0;i<n;i++){
+      sum+=numbers[i]-'0';
+   }
+   cout<<sum<<endl;
+   return 0;
 }
