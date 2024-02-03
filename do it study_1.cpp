28f73ff8a59cf586069c273ae85695d24ee6d461
Haewon K <kaagg7189@naver.com>
2024-02-03 20:41:54 +0900

    third commit

--- do it study_1.cpp
+++ do it study_1.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
-
+//백준 11720 두잇1번
 int main(void) {
-    
+    string numbers;
+    int n;
+    cin>>n;
+    cin>>numbers;
+    int sum=0;
 
+    for(int i=0;i<n;i++){
+        sum+=numbers[i]-'0';
+    }
+    cout<<sum<<endl;
     return 0;
 }
